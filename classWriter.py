f2a8cf8359dbe850f16f41ba16a13a453ad4b981
Thales Lagemann <thalesalaugustol@gmail.com>
2022-05-30 14:13:03 -0300

    Add update files
    Add updated modeloWrites.txt, classWriter.py and crud_example.py files.

--- classWriter.py
+++ classWriter.py
@@ -1,6 +1,7 @@
 import psycopg2
 from dbConnection import dbConnection
 from dbConnectionData import dbConnectionData
+from dbMetadata import dbMetadata
 
 class classWriter():
 
@@ -20,6 +21,12 @@ class classWriter():
             print(error)
 
     def writeInFile(self):
+        mdt = dbMetadata.getTableMetadata(self.c, self.tableName)
+
+        mdt_len = len(mdt)
+
+        print(mdt_len)
+
         try:
             f = open(f'{self.tableName}.py', 'w')
             if(f):
@@ -32,13 +39,37 @@ class classWriter():
         m = open('modeloWrites.txt', 'r')
         text = []
         text = m.readlines()
-
+        identation = '    '
+        
         for line in text:   
             print("Line reads:", line)
-            if line == '__classDeclaration\n':
+            if '__classDeclaration' in line:
                 f.write(f'class {self.tableName}DAO(model):\n')
                 print('Found class declaration.')
                 print(f'Replacing with "class {self.tableName}DAO(model):\\n"')
+            elif '__insertStandartDeclaration' in line and mdt_len is not 0:
+                sent = str()
+                for i in range (3):
+                    sent += f'{identation}'
+                sent += 'cur.execute(f"INSERT INTO {self.tableName} VALUES ({data[0]}'
+                for i in range(mdt_len - 1):
+                    sent += ", {data["
+                    sent += f"{i + 1}]"
+                    sent += "}]"
+                sent += ')")\n'
+                print(f"Replacing __insertStandartDeclaration with:\n {sent}")
+                f.write(sent)
+            elif '__updateStandartDeclaration' in line and mdt_len is not 0:
+                sent = str()
+                for i in range (3):
+                    sent += f'{identation}'
+                sent += 'cur.execute(f"UPDATE {data[0]} WHERE table = \'{self.tableName}\' VALUES ({data[0]}'
+                for i in range(mdt_len - 1):
+                    sent += ", {data["
+                    sent += f"{i + 1}]"
+                    sent += "}]"
+                sent += ')")\n'
+                print(f"Replacing __updateStandartDeclaration with:\n {sent}")
+                f.write(sent)
             else:
                 f.write(line)
-                
